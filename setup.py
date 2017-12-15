3d2b32ee1c9ddb2cd894ec2028d135bdb6931181
Alexei <hello@alexei.ro>
2017-12-15 15:32:10 +0200

    Setup files

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='python-addresslib',
+    packages=['addresslib'],
+    version='1.0.0',
+    description='Python email address parsing library',
+    author='Alexei',
+    author_email='hello@alexei.ro',
+    url='https://github.com/alexei/python-addresslib',
+    download_url='https://github.com/alexei/python-addresslib/archive/1.0.0.tar.gz',  # noqa
+    keywords=['email address parser']
+)
